7f970ad44ce7d59e810a7f8a55b5b8f70ecf617f
F415alarrahman <f415alarr@gmail.com>
2024-12-17 13:42:55 +0700

    setup project

---
+++ lib/view/home/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:agrolyn_web/provider/home_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => HomeNotifier(context: context),
+      child: Consumer<HomeNotifier>(
+        builder: (context, value, child) {
+          return Scaffold();
+        },
+      ),
+    );
+  }
+}
