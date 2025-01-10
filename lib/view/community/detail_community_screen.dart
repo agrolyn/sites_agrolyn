0f66e28e81077e8a3e46f4b5798f918b644ffef3
dodichakill <dodiherlangga91@gmail.com>
2025-01-10 20:21:15 +0700

    fix: responsive detail community

--- lib/view/community/detail_community_screen.dart
+++ lib/view/community/detail_community_screen.dart
@@ -1,5 +1,6 @@
 import 'package:agrolyn_web/provider/community_notifer.dart';
 import 'package:agrolyn_web/utils/inter_prefs.dart';
+import 'package:agrolyn_web/utils/responsive.dart';
 import 'package:agrolyn_web/view/community/content_question_detail.dart';
 import 'package:agrolyn_web/view/community/input_answer.dart';
 import 'package:agrolyn_web/view/community/section_answers.dart';
@@ -8,6 +9,7 @@ import 'package:agrolyn_web/widget/navbar/navbar.dart';
 import 'package:agrolyn_web/widget/navbar/navbar_desktop.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DetailCommunityScreen extends StatelessWidget {
@@ -46,127 +48,89 @@ class DetailCommunityScreen extends StatelessWidget {
                 key: scaffoldKey,
                 backgroundColor: Colors.white,
                 drawer: const NavDrawer(),
-                body: SafeArea(
-                  child: Stack(fit: StackFit.expand, children: [
-                    SingleChildScrollView(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          NavbarDesktop(activePage: ""),
-                          Stack(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.black.withOpacity(0.1),
-                                      blurRadius: 5,
-                                      spreadRadius: 2,
-                                      offset: const Offset(0, 2),
-                                    ),
-                                  ],
-                                ),
-                                child: Column(
-                                  children: [
-                                    // Bagian gambar
-                                    Stack(children: [
-                                      Image.network(
-                                        dataQuestion["question_thumbnail"],
-                                        fit: BoxFit.cover,
-                                        width: double.infinity,
-                                        height: 256,
-                                      ),
-                                    ]),
-                                    // main content
-                                    Padding(
-                                      padding: const EdgeInsets.all(16.0),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          // Konten Detail Pertanyaan + interaksi
-                                          ContentQuestionDetail(
-                                              dataQuestion: dataQuestion,
-                                              likeQuestion: value.likeQuestion,
-                                              dislikeQuestion:
-                                                  value.dislikeQuestion,
-                                              likeNum: likeNum,
-                                              nameUser: name),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              // Tombol "Kembali Ke Halaman Utama Komunitas"
-                              Positioned(
-                                top: 20,
-                                left: 0,
-                                right: 0,
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 16),
-                                  child: SizedBox(
-                                      width: 44,
-                                      height: 44,
-                                      child: InkWell(
-                                        onTap: () async {
-                                          await InterPrefs.init();
-                                          InterPrefs.setPrefs(
-                                              "filterBy", "semua");
-                                          value.reNotifyListeners();
-                                          Navigator.pushReplacementNamed(
-                                              context, '/community');
-                                        },
-                                        child: Container(
-                                            decoration: BoxDecoration(
-                                              color: Colors.black38,
-                                              borderRadius:
-                                                  BorderRadius.circular(40),
-                                              border: Border.all(
-                                                  color: Colors.grey),
-                                            ),
-                                            child: const Row(
-                                              children: [
-                                                SizedBox(
-                                                  width: 15,
-                                                ),
-                                                Icon(
-                                                  Icons.arrow_back_ios,
-                                                  color: Colors.white,
-                                                ),
-                                                Text(
-                                                  "Kembali Ke Halaman Utama Komunitas",
-                                                  style: TextStyle(
-                                                      color: Colors.white,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                )
-                                              ],
-                                            )),
-                                      )),
-                                ),
-                              )
-                            ],
-                          ),
-                          // bagian jawaban
-                          SectionAnswers(
-                            dataQuestion: dataQuestion,
-                            dataAnswer: dataAnswer,
-                            name: name,
-                          )
-                        ],
-                      ),
+                body: SingleChildScrollView(
+                  child: Column(children: [
+                    NavbarDesktop(
+                      activePage: "",
                     ),
-                    const Spacer(),
-                    InputAnswer(
-                      questionId: id,
-                      type: dataQuestion["question_type"] == "jagung"
-                          ? 1
-                          : dataQuestion["question_type"] == "padi"
-                              ? 2
-                              : 3,
+                    ResponsiveRowColumn(
+                      layout:
+                          ResponsiveBreakpoints.of(context).smallerThan(DESKTOP)
+                              ? ResponsiveRowColumnType.COLUMN
+                              : ResponsiveRowColumnType.ROW,
+                      rowCrossAxisAlignment: CrossAxisAlignment.start,
+                      columnCrossAxisAlignment: CrossAxisAlignment.center,
+                      columnMainAxisSize: MainAxisSize.min,
+                      rowPadding: const EdgeInsets.symmetric(
+                          horizontal: 0, vertical: 0),
+                      columnPadding: const EdgeInsets.symmetric(
+                          horizontal: 0, vertical: 0),
+                      children: [
+                        ResponsiveRowColumnItem(
+                          rowFlex: 1,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  blurRadius: 5,
+                                  spreadRadius: 2,
+                                  offset: const Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              children: [
+                                Image.network(
+                                  dataQuestion["question_thumbnail"],
+                                  fit: BoxFit.cover,
+                                  width: double.infinity,
+                                  height: Responsive.isMobile(context)
+                                      ? 256
+                                      : Responsive.heightScreen(context) * 0.69,
+                                ),
+                                // main content
+                                Padding(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      // Konten Detail Pertanyaan + interaksi
+                                      ContentQuestionDetail(
+                                          dataQuestion: dataQuestion,
+                                          likeQuestion: value.likeQuestion,
+                                          dislikeQuestion:
+                                              value.dislikeQuestion,
+                                          likeNum: likeNum,
+                                          nameUser: name),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        ResponsiveRowColumnItem(
+                          rowFlex: 1,
+                          child: Stack(children: [
+                            SectionAnswers(
+                              dataQuestion: dataQuestion,
+                              dataAnswer: dataAnswer,
+                              name: name,
+                            ),
+                            InputAnswer(
+                              questionId: id,
+                              type: dataQuestion["question_type"] == "jagung"
+                                  ? 1
+                                  : dataQuestion["question_type"] == "padi"
+                                      ? 2
+                                      : 3,
+                            )
+                          ]),
+                        )
+                      ],
                     )
                   ]),
                 ));
--- lib/view/community/section_answers.dart
+++ lib/view/community/section_answers.dart
@@ -1,3 +1,4 @@
+import 'package:agrolyn_web/utils/responsive.dart';
 import 'package:agrolyn_web/view/community/card_answer.dart';
 import 'package:flutter/material.dart';
 
@@ -17,59 +18,64 @@ class SectionAnswers extends StatelessWidget {
     return Container(
       color: const Color.fromARGB(255, 244, 243, 243),
       padding: const EdgeInsets.all(16),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const SizedBox(
-            height: 12,
-          ),
-          Row(children: [
-            const Icon(
-              Icons.comment_outlined,
-              size: 16,
-            ),
+      height: Responsive.isMobile(context)
+          ? null
+          : Responsive.heightScreen(context) * 0.9,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
             const SizedBox(
-              width: 8,
+              height: 12,
             ),
-            Text(
-              dataQuestion["number_of_answer"].toString(),
-              style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black87),
-            ),
+            Row(children: [
+              const Icon(
+                Icons.comment_outlined,
+                size: 16,
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              Text(
+                dataQuestion["number_of_answer"].toString(),
+                style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black87),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              const Text(
+                "Jawaban",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black87),
+              ),
+            ]),
+            ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: dataAnswer.length,
+                itemBuilder: (context, index) {
+                  print(dataAnswer[index]);
+                  return CardAnswer(
+                    id: dataAnswer[index]["id"],
+                    answer: dataAnswer[index]["answer"],
+                    username: dataAnswer[index]["username"],
+                    releasedDate: dataAnswer[index]["released_date"],
+                    userProfile: dataAnswer[index]["user_profile"],
+                    likeNum: dataAnswer[index]["like_num"],
+                    questionId: dataQuestion["id"],
+                    name: name!,
+                  );
+                }),
             const SizedBox(
-              width: 8,
-            ),
-            const Text(
-              "Jawaban",
-              style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black87),
-            ),
-          ]),
-          ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: dataAnswer.length,
-              itemBuilder: (context, index) {
-                print(dataAnswer[index]);
-                return CardAnswer(
-                  id: dataAnswer[index]["id"],
-                  answer: dataAnswer[index]["answer"],
-                  username: dataAnswer[index]["username"],
-                  releasedDate: dataAnswer[index]["released_date"],
-                  userProfile: dataAnswer[index]["user_profile"],
-                  likeNum: dataAnswer[index]["like_num"],
-                  questionId: dataQuestion["id"],
-                  name: name!,
-                );
-              }),
-          const SizedBox(
-            height: 50,
-          )
-        ],
+              height: 50,
+            )
+          ],
+        ),
       ),
     );
   }
